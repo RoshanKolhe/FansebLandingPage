ccecf98e5c40e8b7b4441786cd307e4492195b50
RoshanKolhe <kolheroshan508@gmail.com>
2023-01-25 17:29:21 +0530

    added animation to images

--- src/components/Home/MuchMore/MuchMore.js
+++ src/components/Home/MuchMore/MuchMore.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Col, Container, Row} from 'react-bootstrap';
 import Galaxy from '../../Galaxy/Galaxy';
+import {useScrollHandler} from '../Instagram/Instagram';
 import './MuchMore.css';
 
 export default function MuchMore() {
+  const scroll = useScrollHandler();
+  console.log('scroll', scroll);
   return (
     <>
       <div style={{position: 'relative'}}>
@@ -12,7 +15,7 @@ export default function MuchMore() {
             <Row>
               <Col md={12}>
                 <div className="section-title1">
-                  <h2 className="title">
+                  <h2 className="title" style={{color: '#fff'}}>
                     but that’s not all. there’s so much <br />{' '}
                     <span
                       style={{
@@ -23,7 +26,7 @@ export default function MuchMore() {
                         webkitTextFillColor: 'transparent',
                       }}
                     >
-                      treasure
+                      more
                     </span>{' '}
                     you get.
                   </h2>
@@ -31,15 +34,24 @@ export default function MuchMore() {
               </Col>
               <div className="icon-grid animate__zoomIn wow animate__animated">
                 <div className="avatar-card avatar-1">
-                  <img
-                    src="https://d2pas86kykpvmq.cloudfront.net/images/humans-3.0/together_with_digital_icons.png"
-                    width={230}
-                    alt="img"
-                    style={{marginLeft: '90px'}}
-                    className="mb-3 mt-5"
-                  />
+                  <div class="circle">
+                    {scroll >= 1900 ? (
+                      <img
+                        src="https://d2pas86kykpvmq.cloudfront.net/images/humans-3.0/together_with_digital_icons.png"
+                        width={230}
+                        alt="img"
+                        style={{marginLeft: '90px'}}
+                        className={
+                          scroll >= 1900
+                            ? 'animateImage mb-3 mt-5'
+                            : 'mb-3 mt-5'
+                        }
+                      />
+                    ) : null}
+                  </div>
+
                   <p>
-                    {'getting '}
+                    {' '}
                     <span
                       style={{
                         fontWeight: '600',
@@ -49,19 +61,25 @@ export default function MuchMore() {
                         webkitTextFillColor: 'transparent',
                       }}
                     >
-                      treasure
+                      more
                     </span>{' '}
-                    by paid brand collabs
+                    paid brand collabs
                   </p>
                 </div>{' '}
                 <div className="avatar-card avatar-2 animate__zoomIn wow animate__animated">
-                  <img
-                    src="https://cdn3d.iconscout.com/3d/premium/thumb/businessman-working-on-laptop-while-seating-on-sofa-5622801-4694800.png"
-                    width={280}
-                    alt="img"
-                  />
+                  <div class="circle">
+                    {scroll >= 1900 ? (
+                      <img
+                        src="https://cdn3d.iconscout.com/3d/premium/thumb/businessman-working-on-laptop-while-seating-on-sofa-5622801-4694800.png"
+                        width={280}
+                        alt="img"
+                        className={scroll >= 1900 ? 'animateImage' : ''}
+                      />
+                    ) : null}
+                  </div>
+
                   <p>
-                    {'getting '}
+                    {' '}
                     <span
                       style={{
                         fontWeight: '600',
@@ -71,20 +89,28 @@ export default function MuchMore() {
                         webkitTextFillColor: 'transparent',
                       }}
                     >
-                      treasure
+                      more
                     </span>{' '}
-                    by celebrity collabs
+                    celebrity collabs
                   </p>
                 </div>
                 <div className="avatar-card avatar-3 pl-3 animate__zoomIn wow animate__animated">
-                  <img
-                    src="https://ouch-cdn2.icons8.com/w1_iq548AipAMAEcCVq8zsTqFbNY7oEnx3cLpHtj6so/rs:fit:256:297/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNDkz/L2MxYzcxMDk4LTE5/ZWEtNDNjMS1hMzRk/LWFlYzk4ODE1ZWQw/Zi5wbmc.png"
-                    width={180}
-                    alt="img"
-                    className="ml-5"
-                  />
+                  <div class="circle">
+                    {scroll >= 2333 ? (
+                      <img
+                        src="https://ouch-cdn2.icons8.com/w1_iq548AipAMAEcCVq8zsTqFbNY7oEnx3cLpHtj6so/rs:fit:256:297/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNDkz/L2MxYzcxMDk4LTE5/ZWEtNDNjMS1hMzRk/LWFlYzk4ODE1ZWQw/Zi5wbmc.png"
+                        width={130}
+                        alt="img"
+                        className={
+                          scroll >= 2333 ? 'animateImage ml-5' : 'ml-5'
+                        }
+                        style={{marginLeft: '42px'}}
+                      />
+                    ) : null}
+                  </div>
+
                   <p>
-                    refer friends for more{' '}
+                    refer friends for{' '}
                     <span
                       style={{
                         fontWeight: '600',
@@ -94,18 +120,25 @@ export default function MuchMore() {
                         webkitTextFillColor: 'transparent',
                       }}
                     >
-                      treasure
+                      more
                     </span>{' '}
+                    returns
                   </p>
                 </div>{' '}
                 <div className="avatar-card avatar-4 animate__zoomIn wow animate__animated">
-                  <img
-                    src="https://cdn3d.iconscout.com/3d/premium/thumb/male-video-blogger-5571171-4659673.png"
-                    width={210}
-                    alt="img"
-                  />
+                  <div class="circle">
+                    {scroll >= 2333 ? (
+                      <img
+                        src="https://cdn3d.iconscout.com/3d/premium/thumb/male-video-blogger-5571171-4659673.png"
+                        width={180}
+                        alt="img"
+                        className={scroll >= 2333 ? 'animateImage' : ''}
+                      />
+                    ) : null}
+                  </div>
+
                   <p>
-                    create your own brand & explore{' '}
+                    launch your own brand &{' '}
                     <span
                       style={{
                         fontWeight: '600',
@@ -115,7 +148,7 @@ export default function MuchMore() {
                         webkitTextFillColor: 'transparent',
                       }}
                     >
-                      treasure
+                      more
                     </span>
                   </p>
                 </div>
